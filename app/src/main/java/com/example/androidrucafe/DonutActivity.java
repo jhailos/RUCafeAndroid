507f4327170edb1b6bae4b7562fbea47b8cde61f
pekel <jasonhailos2150@gmail.com>
2024-04-15 18:02:04 -0400

    Able to move between most views (except from donut to home)

--- app/src/main/java/com/example/androidrucafe/DonutActivity.java
+++ app/src/main/java/com/example/androidrucafe/DonutActivity.java
@@ -1,6 +1,8 @@
 package com.example.androidrucafe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,5 +16,10 @@ public class DonutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donut);
+    }
+
+    public void goToHomeView(View view) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
