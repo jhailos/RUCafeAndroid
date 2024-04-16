da9d0b98494f2393ec3cc8f9b56041933bf1454a
pekel <jasonhailos2150@gmail.com>
2024-04-15 21:10:50 -0400

    add full internal functionality to SandwichActivity (excluding placing order)

--- app/src/main/java/com/example/androidrucafe/SandwichActivity.java
+++ app/src/main/java/com/example/androidrucafe/SandwichActivity.java
@@ -3,12 +3,18 @@ package com.example.androidrucafe;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SandwichActivity extends AppCompatActivity {
+    private Spinner quantitySelect;
 
     /**
      * Perform initial setup
@@ -18,25 +24,113 @@ public class SandwichActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sandwich);
+        // initialize quantitySelect spinner listener
+        quantitySelect = findViewById(R.id.quantitySelect);
+        quantitySelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                displaySubtotal(view);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent){
+                // do nothing
+            }
+        });
     }
 
+    /**
+     * get a string of the currently selected bread option
+     * @return
+     */
     private String getBread() {
         RadioGroup radioGroup = findViewById(R.id.breadRadioGroup);
         int selectedId = radioGroup.getCheckedRadioButtonId();
         RadioButton selectedRadioButton = findViewById(selectedId);
+        if (selectedRadioButton == null) return null;
         return selectedRadioButton.getText().toString();
     }
 
+    /**
+     * get a string of the currently selected protein option
+     * @return
+     */
     private String getProtein() {
         RadioGroup radioGroup = findViewById(R.id.proteinRadioGroup);
         int selectedId = radioGroup.getCheckedRadioButtonId();
         RadioButton selectedRadioButton = findViewById(selectedId);
+        if (selectedRadioButton == null) return null;
         return selectedRadioButton.getText().toString();
+    }
+
+    /**
+     * Get the state of the cheese CheckBox
+     * @return true if checked
+     */
+    private boolean getCheese() {
+        CheckBox checkBox = findViewById(R.id.cheese);
+        return checkBox.isChecked();
+    }
+
+    /**
+     * Get the state of the lettuce CheckBox
+     * @return true if checked
+     */
+    private boolean getLettuce() {
+        CheckBox checkBox = findViewById(R.id.lettuce);
+        return checkBox.isChecked();
+    }
+
+    /**
+     * Get the state of the tomato CheckBox
+     * @return true if checked
+     */
+    private boolean getTomato() {
+        CheckBox checkBox = findViewById(R.id.tomato);
+        return checkBox.isChecked();
+    }
+
+    /**
+     * Get the state of the onion CheckBox
+     * @return true if checked
+     */
+    private boolean getOnion() {
+        CheckBox checkBox = findViewById(R.id.onion);
+        return checkBox.isChecked();
+    }
+
+    /**
+     * Get an integer of the currenty selected order quantity
+     * @return
+     */
+    private int getQuantity() {
+        return Integer.parseInt((String) quantitySelect.getSelectedItem());
+    }
+
+    /**
+     * Check if the order is missing necessary fields to calculate
+     * a subtotal or place an order.
+     * @return true if missing necessary fields (bread, protein, quantity)
+     */
+    private boolean missingNecessaryFields() {
+        if (getBread() == null) return true;
+        if (getProtein() == null) return true;
+        return false;
+    }
+
+    /**
+     * Displays the subtotal in GUI
+     */
+    public void displaySubtotal(View view) {
+        if (missingNecessaryFields()) return; // can't calculate, missing info
+        Sandwich order = new Sandwich(getQuantity(), getProtein(), getBread(), getCheese(),
+                getLettuce(), getTomato(), getOnion());
+        TextView subtotalText = findViewById(R.id.subtotalValue);
+        subtotalText.setText("$ " + String.valueOf(order.price()));
     }
 
     public void addToOrder(View view) {
         String bread = getBread();
         String protein = getProtein();
-
+        // INCOMPLETE!
     }
 }
